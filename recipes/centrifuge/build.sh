348531915a0da615a80b8ec42270d4c04b9fd6d0
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2022-03-17 16:08:27 -0400

    Update centrifuge to 1.0.4
    auto-convert evaluation scripts to python 3 with 2to3

--- recipes/centrifuge/build.sh
+++ recipes/centrifuge/build.sh
@@ -7,4 +7,5 @@ mkdir -p $PREFIX/bin
 make CXX=$CXX RELEASE_FLAGS="$CXXFLAGS"
 make install prefix=$PREFIX
 
+2to3 --write --nobackups evaluation/*.py
 cp evaluation/{centrifuge_evaluate.py,centrifuge_simulate_reads.py} $PREFIX/bin
